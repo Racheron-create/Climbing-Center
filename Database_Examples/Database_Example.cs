82b8fcb89b7053af474af1fa36b01279907d14fc
Jamie Wright <45731586+JamieRWright@users.noreply.github.com>
2021-01-23 18:52:05 +0000

    This is a new branch with db examples

---
+++ Database_Examples/Database_Example.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlTypes;
+
+namespace Databse_Example
+{
+    class Database_Example
+    {
+        static void Main(string[] args)
+        {
+            string conString = "Provider=SQLOLEDB;Data Source=DESKTOP-MRJEJE6\\SQLEXPRESS;Initial Catalog=ConnectionDb;Integrated Security=SSPI";
+            DataRow CurrentRow;
+
+            DataTable _tblCustomer = new DataTable();
+            string Customer = "SELECT * FROM Customer";
+            OleDbDataAdapter _daTblCustomers = new OleDbDataAdapter(Customer, conString);
+            _tblCustomer.Clear();
+            _daTblCustomers.Fill(_tblCustomer);
+            //OleDbCommandBuilder _cbC = new OleDbCommandBuilder(_daTblCustomers);
+
+            CurrentRow = _tblCustomer.Rows[0];
+
+            Console.WriteLine((string)CurrentRow["CustomerName"]);
+
+        }
+    }
+}
